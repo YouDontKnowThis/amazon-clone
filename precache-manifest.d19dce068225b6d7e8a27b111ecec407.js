7ca215700c6a1f492a7402bd0638c6c46ead60a6
bk dev <fox.physics@outlook.fr>
2020-10-14 07:40:37 +0100

    Updates

---
+++ precache-manifest.d19dce068225b6d7e8a27b111ecec407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066a06e182e150b45c33ca50efa55878",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "096f01462b06272bcf6e",
+    "url": "/amazon-clone/static/css/2.47f897bf.chunk.css"
+  },
+  {
+    "revision": "485ad4e906fa42f1293e",
+    "url": "/amazon-clone/static/css/main.d1ce3f9f.chunk.css"
+  },
+  {
+    "revision": "096f01462b06272bcf6e",
+    "url": "/amazon-clone/static/js/2.ef5b67b8.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/amazon-clone/static/js/2.ef5b67b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485ad4e906fa42f1293e",
+    "url": "/amazon-clone/static/js/main.5eca0cd9.chunk.js"
+  },
+  {
+    "revision": "89ad3e3c7860c4addbe4",
+    "url": "/amazon-clone/static/js/runtime-main.be9ac9e6.js"
+  }
+]);
